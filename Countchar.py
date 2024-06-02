869b0b73b284350879a1bd0c76b80ebb27a40b01
Anuva13 <anuvadas@hotmail.com>
2024-06-02 22:46:32 +0530

    Addressed review comments

--- Countchar.py
+++ Countchar.py
@@ -1,17 +1,17 @@
 # write a program to count the number of times a letter is appearing in the string
 def main():
-    a = input("enter a string\n")
-    b = input("enter the character for which you want to find its number of occurrence in the string\n")
-    c = 0
-    l = len(a)
-    i = 0
+    User_input_string = input("enter a string\n")
+    User_input_character = input("enter the character for which you want to find its number of occurrence in the string\n")
+    Count_of_character_occurence = 0
+    length_of_string = len(a)
+    Counter = 0
 
-    while (i<l):
+    while (Counter<length_of_string):
         
-        if(a[i]==b):
-            c = c + 1
-        i = i + 1
-    print (f"Number of occurrence of letter {b} {c}")
+        if(User_input_strin[Counter]==User_input_character):
+            Count_of_character_occurence = Count_of_character_occurence + 1
+        Counter = Counter + 1
+    print (f"Number of occurrence of letter {User_input_character} {Count_of_character_occurence}")
 
 if __name__ == ("__main__"):
     main()
