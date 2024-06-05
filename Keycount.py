c5e170c1a7520696b76197c98d29e78520bd4552
Anuva13 <anuvadas@hotmail.com>
2024-06-05 19:30:09 +0530

    This is my first program of the day

---
+++ Keycount.py
@@ -0,0 +1,8 @@
+# WAP to count the number of keys in a dictionary
+def main():
+    Dict_1 = {"one":1,"two":2,"three":3}
+    
+    print(Dict_1.__len__())
+    
+if __name__ == "__main__":
+    main()
