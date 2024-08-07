7198cfd3cd23d3e58b25054aa206570e6f479a02
Anuva13 <anuvadas@hotmail.com>
2024-08-07 09:53:10 +0530

    This commit is to save the module chapter example

---
+++ my_module.py
@@ -0,0 +1,17 @@
+def main():
+    
+
+def say_hi(name):
+    """
+    This function prints a greeting message to the caller
+    """
+    print("Hello",name)
+    
+def sum(x,y,z):
+    """
+    This function adds up three numbers
+    """
+    return (x+y+z)
+
+if __name__ == "__main__":
+    main()
---
+++ my_program.py
@@ -0,0 +1,9 @@
+def main():
+    import my_module
+    
+    my_module.say_hi("Anuva")
+    print(my_module.sum(1,2,3))
+   
+    
+if __name__ == "__main__":
+    main()
