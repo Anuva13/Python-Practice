f34ae7e217a03be4005baae44144630b2f03dc74
Anuva13 <anuvadas@hotmail.com>
2025-03-26 10:33:51 -0400

    Minor changes

--- PythonDataProcessing/hierarchical_indices.py
+++ PythonDataProcessing/hierarchical_indices.py
@@ -2,6 +2,19 @@ import pandas as pd
 from io import StringIO
 
 def main():
+    """
+    Concepts Covered:
+    Reading CSV data from a string.
+    Using multi-indexing in Pandas (date and language).
+    Sorting a multi-indexed DataFrame.
+    Index-based slicing using .loc[]:
+    Retrieving all records for a given date.
+    Retrieving a specific record for a date-language pair.
+    
+    """
+    
+    
+    
     str_data = """date,language,ex_complete
 2017-01-01,python,6
 2017-01-02,python,5
@@ -12,7 +25,10 @@ def main():
 """
     
     df = pd.read_csv(StringIO(str_data))
+    
+    #Sets a multi-index using date and language
     df.set_index(['date', 'language'], inplace=True)
+    #Sorts the DataFrame by the multi-index to ensure proper slicing
     df.sort_index(inplace=True)
 
     print(df.index)
