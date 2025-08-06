1839770468ab60d0e5a1cd4a320904ac37381c3c
Anuva13 <anuvadas@hotmail.com>
2025-08-06 12:50:12 -0400

    Learning Zip and Map functions

--- PythonBasics/List_comprehension.py
+++ PythonBasics/List_comprehension.py
@@ -1,10 +1,10 @@
 def main():
     
-    #multiple_WO()
-    #multiple_W()
-    #newlist()
+    multiple_WO()
+    multiple_W()
+    newlist()
     multiple_3()    
-"""
+
 def multiple_WO():
     multiples = []
     for x in range(1,11):
@@ -19,7 +19,7 @@ def newlist():
     languages = ['Ruby', 'Go,', 'Python', 'Java', 'C', 'Perl']
     length_language = [len(language) for language in languages]
     print(length_language)   
-""" 
+ 
 def multiple_3():
     z = [x for x in range(0,101) if x%3==0]
     print(z)
